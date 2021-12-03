ceeeecfd1a4eaf481e559866a0f1e9e720f19532
TSS-CASAL <t.silvatss@hotmail.com>
2021-12-03 15:08:00 -0300

    dados do data-table estão do JS

--- script.js
+++ script.js
@@ -56,3 +56,31 @@ const Transaction = {
         
     }
 }
+
+const DOM = {
+    transactionsContainer: document.querySelector("#data-table tbody"),
+
+    addTransaction (transaction, index) {
+        console.log(transaction)
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction)
+
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction (transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
+
+        const html = `
+            <td class="description">${transaction.description}</td>
+            <td class= ${CSSclass}>${transaction.amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td><img src="./assets/minus.svg" alt="Remover"></td>
+        </tr>`
+        
+        return html
+    }
+}
+
+transactions.forEach(function (transaction) {
+    DOM.addTransaction(transaction)
+})
